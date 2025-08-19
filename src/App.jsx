ca060204d96c6ae4b92bb9619acee161442d26d0
Dot Phillips <hey@dotty.cool>
2025-08-19 01:06:38 -0700

    refactor to use tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,36 @@
 import React from 'react';
-import './App.css';
 
 import { FaLinkedin, FaGithub, FaItchIo } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="App">
-      <main>
-        <h1>
-          <b>&lt;</b>
+    <div className="grid place-items-center min-h-screen max-w-[45em] mx-auto my-2">
+      <main className="relative text-white rounded-lg bg-black/75 mx-2 mb-40 p-3 pb-32 max-[550px]:pb-20 max-w-full overflow-hidden border-2 border-black text-3xl text-center font-simply-best text-shadow-lg tv-static">
+        <h1 className="inline-block mb-4 pt-20 max-[550px]:pt-10 font-mini-pizza text-8xl max-[550px]:text-5xl">
+          <b className="inline-block animate-up-down text-outline-red" style={{animationDelay: '0s'}}>&lt;</b>
           &nbsp;
-          <b>H</b>
-          <b>e</b>
-          <b>y</b>
-          <b>
+          <b className="inline-block animate-up-down text-outline-orange" style={{animationDelay: '0.1s'}}>H</b>
+          <b className="inline-block animate-up-down text-outline-green" style={{animationDelay: '0.2s'}}>e</b>
+          <b className="inline-block animate-up-down text-outline-cyan" style={{animationDelay: '0.3s'}}>y</b>
+          <b className="inline-block animate-up-down text-outline-blue" style={{animationDelay: '0.4s'}}>
             <i>!</i>
           </b>
-          <b>
+          <b className="inline-block animate-up-down text-outline-purple" style={{animationDelay: '0.5s'}}>
             <i>!</i>
           </b>
           &nbsp;
-          <b>&gt;</b>
+          <b className="inline-block animate-up-down text-outline-pink" style={{animationDelay: '0.6s'}}>&gt;</b>
         </h1>
         <br />
         <p>The biggest dog around making websites and games! 😎🤙</p>
         <br />
-        <a href="https://www.linkedin.com/in/cooldotty/">
+        <a href="https://www.linkedin.com/in/cooldotty/" className="inline-block text-inherit m-2 text-4xl hover:scale-110 transition-transform">
           <FaLinkedin />
         </a>
-        <a href="https://github.com/CoolDotty">
+        <a href="https://github.com/CoolDotty" className="inline-block text-inherit m-2 text-4xl hover:scale-110 transition-transform">
           <FaGithub />
         </a>
-        <a href="https://cooldotty.itch.io/">
+        <a href="https://cooldotty.itch.io/" className="inline-block text-inherit m-2 text-4xl hover:scale-110 transition-transform">
           <FaItchIo />
         </a>
       </main>
---
+++ tailwind.config.js
@@ -0,0 +1,54 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "./public/index.html"
+  ],
+  theme: {
+    extend: {
+      animation: {
+        'gradients': 'gradients 60s ease infinite',
+        'up-down': 'upDown 0.5s alternate infinite ease-in-out',
+        'scroll': 'scroll 3s linear infinite',
+      },
+      keyframes: {
+        gradients: {
+          '0%, 100%': { backgroundPosition: '0% 50%' },
+          '50%': { backgroundPosition: '100% 50%' },
+        },
+        upDown: {
+          'to': { transform: 'translateY(-0.25em)' },
+        },
+        scroll: {
+          '0%': { transform: 'rotate(0.5deg) translate(0, 0rem)' },
+          '50%': { transform: 'rotate(0.25deg) translate(0, 1rem)' },
+          '100%': { transform: 'rotate(0.5deg) translate(0, 2rem)' },
+        }
+      },
+      fontFamily: {
+        'mini-pizza': ['Mini Pizza', 'cursive'],
+        'simply-best': ['Simply the Best', 'cursive'],
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+      textShadow: {
+        'lg': '2px 2px 4px #000000',
+      }
+    },
+  },
+  plugins: [
+    function({ addUtilities }) {
+      const newUtilities = {
+        '.text-shadow': {
+          'text-shadow': '2px 2px 4px #000000',
+        },
+        '.text-shadow-lg': {
+          'text-shadow': '2px 2px 4px #000000',
+        },
+      }
+      addUtilities(newUtilities)
+    }
+  ],
+}
