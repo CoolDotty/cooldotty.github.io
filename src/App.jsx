b2ccee68fdd01b46e5199ceff2028fc91403f6a3
Dot Phillips <hey@dotty.cool>
2025-08-19 21:28:13 -0700

    Add prettier config for tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -4,33 +4,73 @@ import { FaLinkedin, FaGithub, FaItchIo } from 'react-icons/fa';
 
 function App() {
   return (
-    <div className="grid place-items-center min-h-screen max-w-[45em] mx-auto my-2">
-      <main className="relative text-white rounded-lg bg-black/75 mx-2 mb-40 p-3 pb-32 max-[550px]:pb-20 max-w-full overflow-hidden border-2 border-black text-3xl text-center font-simply-best text-shadow-lg tv-static">
-        <h1 className="inline-block mb-4 pt-20 max-[550px]:pt-10 font-mini-pizza text-8xl max-[550px]:text-5xl">
-          <b className="inline-block animate-up-down text-outline-red" style={{animationDelay: '0s'}}>&lt;</b>
-          &nbsp;
-          <b className="inline-block animate-up-down text-outline-orange" style={{animationDelay: '0.1s'}}>H</b>
-          <b className="inline-block animate-up-down text-outline-green" style={{animationDelay: '0.2s'}}>e</b>
-          <b className="inline-block animate-up-down text-outline-cyan" style={{animationDelay: '0.3s'}}>y</b>
-          <b className="inline-block animate-up-down text-outline-blue" style={{animationDelay: '0.4s'}}>
-            <i>!</i>
-          </b>
-          <b className="inline-block animate-up-down text-outline-purple" style={{animationDelay: '0.5s'}}>
-            <i>!</i>
+    <div className="mx-auto my-2 grid min-h-screen max-w-[45em] place-items-center">
+      <main className="tv-static relative mx-2 mb-40 max-w-full overflow-hidden rounded-lg border-2 border-black bg-black/75 p-3 pb-32 text-center font-simply-best text-3xl text-white text-shadow-lg max-[550px]:pb-20">
+        <h1 className="mb-4 inline-block pt-20 font-mini-pizza text-8xl max-[550px]:pt-10 max-[550px]:text-5xl">
+          <b
+            className="text-outline-red inline-block animate-up-down"
+            style={{ animationDelay: '0s' }}
+          >
+            &lt;
           </b>
           &nbsp;
-          <b className="inline-block animate-up-down text-outline-pink" style={{animationDelay: '0.6s'}}>&gt;</b>
+          <b
+            className="text-outline-orange inline-block animate-up-down"
+            style={{ animationDelay: '0.1s' }}
+          >
+            H
+          </b>
+          <b
+            className="text-outline-green inline-block animate-up-down"
+            style={{ animationDelay: '0.2s' }}
+          >
+            e
+          </b>
+          <b
+            className="text-outline-cyan inline-block animate-up-down"
+            style={{ animationDelay: '0.3s' }}
+          >
+            y
+          </b>
+          <b
+            className="text-outline-blue inline-block animate-up-down"
+            style={{ animationDelay: '0.4s' }}
+          >
+            <i>!</i>
+          </b>
+          <b
+            className="text-outline-purple inline-block animate-up-down"
+            style={{ animationDelay: '0.5s' }}
+          >
+            <i>!</i>
+          </b>
+          &nbsp;
+          <b
+            className="text-outline-pink inline-block animate-up-down"
+            style={{ animationDelay: '0.6s' }}
+          >
+            &gt;
+          </b>
         </h1>
         <br />
         <p>The biggest dog around making websites and games! 😎🤙</p>
         <br />
-        <a href="https://www.linkedin.com/in/cooldotty/" className="inline-block text-inherit m-2 text-4xl hover:scale-110 transition-transform">
+        <a
+          href="https://www.linkedin.com/in/cooldotty/"
+          className="m-2 inline-block text-4xl text-inherit transition-transform hover:scale-110"
+        >
           <FaLinkedin />
         </a>
-        <a href="https://github.com/CoolDotty" className="inline-block text-inherit m-2 text-4xl hover:scale-110 transition-transform">
+        <a
+          href="https://github.com/CoolDotty"
+          className="m-2 inline-block text-4xl text-inherit transition-transform hover:scale-110"
+        >
           <FaGithub />
         </a>
-        <a href="https://cooldotty.itch.io/" className="inline-block text-inherit m-2 text-4xl hover:scale-110 transition-transform">
+        <a
+          href="https://cooldotty.itch.io/"
+          className="m-2 inline-block text-4xl text-inherit transition-transform hover:scale-110"
+        >
           <FaItchIo />
         </a>
       </main>
